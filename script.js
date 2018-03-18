8fc47c89d5fcf5a7ee2018c5bc193f7589976094
eddy <eddy.salibi@supinternet.fr>
2018-03-18 18:42:20 +0100

    still on page 2 respnsive, did page 3 not done woth javascript

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
 window.onload = function(){
-      document.querySelector('.menu-button').onclick = function(){
+     //page 1 navigation
+    $('.menu-button').click(function(){
        $('nav ul').toggleClass('show');
-    }
+    })
+    $(".home").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#page1").offset().top},
+            'slow');
+    });
 
+    $(".serv").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#page2").offset().top},
+            'slow');
+    });
+    $(".port").click(function() {
+        $('html,body').animate({
+            scrollTop: $("#page3").offset().top},
+            'slow');
+    });
+
+
+
+
+    //page 3 portfolio
+
+    $('.all').click(function(){  
+        $('#photos').css("display","show");
+    })
+    $('.pana').click(function(){  
+        $('.p1' ).css("display","");
+    })
+    $('.portrail').click(function(){  
+        $('.photos').css("display","show");
+    })
+    $('.macro').click(function(){  
+        $('.photos').css("display","show");
+    })
+    $('.journal').click(function(){  
+        $('.photos').css("display","show");
+    })
+    
 }
