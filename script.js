facde0a2894b2cb64f023ecff0e474cf6aad5d8a
eddy <eddy.salibi@supinternet.fr>
2018-03-23 16:37:16 +0100

    added porfolio mobile version

--- script.js
+++ script.js
@@ -1,44 +1,14 @@
-window.onload = function(){
-     //page 1 navigation
-    $('.menu-button').click(function(){
-       $('nav ul').toggleClass('show');
-    })
-    $(".home").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#page1").offset().top},
-            'slow');
-    });
+window.onload = function () {
+    //page 1  mobile navigation
 
-    $(".serv").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#page2").offset().top},
-            'slow');
-    });
-    $(".port").click(function() {
-        $('html,body').animate({
-            scrollTop: $("#page3").offset().top},
-            'slow');
-    });
+    document.querySelector('.menu-button').onclick = function () {
+        console.log("sada");
+
+        document.querySelector('nav ul').classList.toggle("show");
 
 
-
-
-    //page 3 portfolio
-
-    $('.all').click(function(){  
-        $('#photos').css("display","show");
-    })
-    $('.pana').click(function(){  
-        $('.p1' ).css("display","");
-    })
-    $('.portrail').click(function(){  
-        $('.photos').css("display","show");
-    })
-    $('.macro').click(function(){  
-        $('.photos').css("display","show");
-    })
-    $('.journal').click(function(){  
-        $('.photos').css("display","show");
-    })
-    
+    }
+    // $('.menu-button').click(function(){
+    //   $('nav ul').toggleClass('show');
+    //  })
 }
